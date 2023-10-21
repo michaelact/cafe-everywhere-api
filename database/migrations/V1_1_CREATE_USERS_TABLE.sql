465a8e279d6618c36c2cde6012a30b07d34ee247
michaelact <86778470+michaelact@users.noreply.github.com>
2023-10-21 21:25:19 +0700

    chore(user): make email column unique and add index

--- database/migrations/V1_1_CREATE_USERS_TABLE.sql
+++ database/migrations/V1_1_CREATE_USERS_TABLE.sql
@@ -1,9 +1,13 @@
+-- Adding an table for users
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW() NOT NULL,
     deleted_at TIMESTAMP,
     is_active BOOLEAN DEFAULT true
 );
+
+-- Adding an index for the email column
+CREATE INDEX idx_users_email ON users (email);
